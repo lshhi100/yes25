890fed5fcaaa2615d79e0c07761322968b2156aa
lshhi5 <lshhi100@gmail.com>
2022-03-14 10:08:23 +0900

    Initial commit

---
+++ src/main/java/com/lshhi5/yes25/Yes25Application.java
@@ -0,0 +1,13 @@
+package com.lshhi5.yes25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yes25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lshhi5/yes25/Yes25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lshhi5.yes25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yes25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
